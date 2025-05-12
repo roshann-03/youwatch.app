6924c40294f97d32f1447bd770c921e0b02a8b50
roshan <mrroshan145@gmail.com>
2025-05-12 18:50:32 +0530

    logo update

--- youwatch-frontend/src/components/Navbar/LoggedInNav.jsx
+++ youwatch-frontend/src/components/Navbar/LoggedInNav.jsx
@@ -102,7 +102,7 @@ const LoggedInNav = ({ onLogout }) => {
             <div className="logo-container flex justify-center items-center">
               <div className="logo h-10 w-14 rounded-lg flex justify-center items-center object-center object-cover">
                 <img
-                  src={`https://yt3.googleusercontent.com/CfT3aY2_zoY1XcDZYvOTmPzaNGwfOMzwQOlcgOCeHDDqf4vxqjkd9XXtIURS-BolPpQqHTXL=s900-c-k-c0x00ffffff-no-rj`}
+                  src={`/logo.jpg`}
                   alt=""
                   className="h-full w-full rounded-lg"
                 />
