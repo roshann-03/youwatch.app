20c6722f8f0620743feffe5b96fa2bd808965822
roshann-03 <mrroshan145@gmail.com>
2025-05-15 01:10:07 +0530

    cors update

--- youwatch-backend/src/app.js
+++ youwatch-backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(morgan("combined"));
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "https://youwatch-api.onrender.com",
     credentials: true,
     // sameSite: "None",
   })
